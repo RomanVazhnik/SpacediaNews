cf6559274b3c92d27077320479309d619bef3c8e
BYCHKOVSKIY <fury21@mail.ru>
2019-08-29 12:20:50 +0300

    Initial Commit

---
+++ Spacedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spacedia
+//
+//  Created by Александр Б. on 29/08/2019.
+//  Copyright © 2019 Александр Б. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
