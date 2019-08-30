347d26f9e36d9d5acb083468d20f8b1ca8a81466
Roman <vazhnik.roma@mail.ru>
2019-08-30 13:46:42 +0300

    Добавил структуры для json

---
+++ Spacedia/Model/Links.swift
@@ -0,0 +1,18 @@
+//
+//  Links.swift
+//  Spacedia
+//
+//  Created by Роман Важник on 30/08/2019.
+//  Copyright © 2019 Александр Б. All rights reserved.
+//
+
+import Foundation
+
+struct Links {
+    let missionPatch: String
+    let missionPatchSmall: String
+    let wikipedia: String
+}
+
+// missionPatch для отображение на подробном экране, а missionPatchSmall для
+// таблицы 
