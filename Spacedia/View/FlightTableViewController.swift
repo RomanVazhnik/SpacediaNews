7e2b78dfcc3754c12da7dfc6b07620ab3dde1aea
Roman <vazhnik.roma@mail.ru>
2019-08-30 17:30:26 +0300

    добавил метод для загрузки картинки по url(fetchData), сделал dataSource

--- Spacedia/View/FlightTableViewController.swift
+++ Spacedia/View/FlightTableViewController.swift
@@ -10,34 +10,15 @@ import UIKit
 
 class FlightTableViewController: UITableViewController {
 
+    var flights: [Flight] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        NetworkingManager.fetchData(for: "https://api.spacexdata.com/v3/launches/") { (flights) in
+            if let flights = flights {
+                self.flights = flights
+            }
+        }
     }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FlightCell", for: indexPath) as! FlightCell
-
-        // Configure the cell...
-
-        return cell
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
