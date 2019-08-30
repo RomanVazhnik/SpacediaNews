bba18f93f6e32600ac110e9a80ad44ed0e6b6c14
Bogdan <bgdnbskkv@gmail.com>
2019-08-30 22:09:24 +0500

    Image Outlet

--- Spacedia/View/FlightCell.swift
+++ Spacedia/View/FlightCell.swift
@@ -13,11 +13,13 @@ class FlightCell: UITableViewCell {
     @IBOutlet var flightNumber: UILabel!
     @IBOutlet var missionName: UILabel!
     @IBOutlet var flightDate: UILabel!
+    @IBOutlet var flightImage: UIImageView!
     
     func configure(with flight: Flight) {
         
-        flightNumber.text = "№\(flight.flightNumber ?? 0)"
+        flightNumber.text = "\(flight.flightNumber ?? 0)"
         missionName.text = flight.missionName ?? "Имя не найдено"
         flightDate.text = "\(flight.launchDateUnix ?? 0)"
+        flightImage.image = #imageLiteral(resourceName: "default")
     }
 }
