62cc67f7ad2d7c8a710d85a923d81437dff29f85
Bogdan <bgdnbskkv@gmail.com>
2019-08-30 18:56:55 +0500

    Класс для таблицы Класс для ячейки Сториборд для полетов

---
+++ Spacedia/View/FlightCell.swift
@@ -0,0 +1,23 @@
+//
+//  FlightCell.swift
+//  Spacedia
+//
+//  Created by Bogdan Baskakov on 30/08/2019.
+//  Copyright © 2019 Александр Б. All rights reserved.
+//
+
+import UIKit
+
+class FlightCell: UITableViewCell {
+    
+    @IBOutlet var flightNumber: UILabel!
+    @IBOutlet var missionName: UILabel!
+    @IBOutlet var flightDate: UILabel!
+    
+    func configure(with flight: Flight) {
+        
+        flightNumber.text = "№\(flight.flightNumber ?? 0)"
+        missionName.text = flight.missionName ?? "Имя не найдено"
+        flightDate.text = "\(flight.launchDateUnix ?? 0)"
+    }
+}
